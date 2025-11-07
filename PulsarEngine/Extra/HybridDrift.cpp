be1b3733f2f163baac69bbe38d56307b11bba095
ZPL <158531259+ZacharyPL@users.noreply.github.com>
2025-11-07 16:53:20 -0600

    Fix Hybrid Drift bug for outside drift bikes

--- PulsarEngine/Extra/HybridDrift.cpp
+++ PulsarEngine/Extra/HybridDrift.cpp
@@ -2,10 +2,12 @@
 
 namespace HybridDrift {
 
-// original code by Ismy and CLF78.
+// original code by Ismy and CLF78, with fixes by Ro.
 asmFunc GetHybridDrift1() {
     ASM(
         lwz r0, 0x14(r3);
+
+        // Check if we're an inside drifting, if we are, end the code (Bug is only for outside drift bikes)
         lwz r12, 0(r28);
         lwz r12, 0(r12);
         lwz r12, 0x14(r12);
@@ -13,7 +15,18 @@ asmFunc GetHybridDrift1() {
         lwz r12, 0x4(r12);
         cmpwi r12, 2;
         beqlr;
-        li r0, 0;
+
+        // Check if we're a kart, if we are, skip to the end
+        cmpwi r12, 0;
+        beq skip;
+
+        // Check if we're drifting, if we are, end the code
+        outsidefix : lwz r3, 4(r3);
+        andis.r3, r3, 0x1000;
+        bnelr;
+
+        // Fix slide issue (Load a dummy value to force the next branch)
+        skip : li r0, 0;
         blr;)
 }
 kmCall(0x8057930C, GetHybridDrift1);
