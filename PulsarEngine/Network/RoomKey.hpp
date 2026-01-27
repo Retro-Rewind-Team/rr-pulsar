c0c17fa4f5d1ac3966293f4711624689ec084628
ZPL <158531259+ZacharyPL@users.noreply.github.com>
2026-01-27 15:33:32 -0600

    Update RoomKey.hpp

--- PulsarEngine/Network/RoomKey.hpp
+++ PulsarEngine/Network/RoomKey.hpp
@@ -6,9 +6,9 @@
 namespace Pulsar {
 namespace Network {
 
-static const u32 HARD_CODED_ROOM_KEY = 0x12345678; // Placeholder for hardcoded room key, the actual RR key is private
+extern u32 HARD_CODED_ROOM_KEY;
 
-} // namespace Network
-} // namespace Pulsar
+}  // namespace Network
+}  // namespace Pulsar
 
 #endif
