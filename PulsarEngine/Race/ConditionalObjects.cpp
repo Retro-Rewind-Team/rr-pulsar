715144f25e836294c5872448a17e3e5b9d9f147d
ZPL <158531259+ZacharyPL@users.noreply.github.com>
2026-02-18 19:29:15 -0600

    Read original calc to COBJ

--- PulsarEngine/Race/ConditionalObjects.cpp
+++ PulsarEngine/Race/ConditionalObjects.cpp
@@ -611,7 +611,8 @@ static void ConditionalObjectModelUpdate(Object* object) {
 }
 kmCall(0x8082aa20, ConditionalObjectModelUpdate);  // Object::UpdateModel call in ObjectsMgr::Update
 
-static void ConditionalProcessAllAndCalcKCL(void* kclMgr) {
+kmRuntimeUse(0x8081b618);
+static void ConditionalProcessAllAndCalcKCL(void* kclMgr, ObjectsMgr& objectsMgr) {
     if (kclMgr != nullptr) {
         const u16 kclCount = *reinterpret_cast<const u16*>(reinterpret_cast<const u8*>(kclMgr) + 0x4);
         Object** kclObjects = *reinterpret_cast<Object***>(reinterpret_cast<u8*>(kclMgr) + 0x8);
@@ -625,6 +626,9 @@ static void ConditionalProcessAllAndCalcKCL(void* kclMgr) {
             ApplyPerScreenVisibility(*obj, state);
         }
     }
+    typedef void (*OriginalCalcFn)(void*);
+    OriginalCalcFn originalCalc = reinterpret_cast<OriginalCalcFn>(kmRuntimeAddr(0x8081b618));
+    originalCalc(kclMgr);
 }
 kmCall(0x8082aa40, ConditionalProcessAllAndCalcKCL);  // ObjectDriveableDirector::calc call in ObjectsMgr::Update
 
