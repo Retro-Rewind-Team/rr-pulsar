976108723b037ae4082706b78b27c240d27a8ea9
y21 <30553356+y21@users.noreply.github.com>
2026-03-19 19:00:29 +0100

    implement a simple ABI version system to check compatibility with the channel
    The idea is we have an int that we increment each time that we need to
    make changes that break compat, such as needed in this PR (which should
    be rare). Using mismatched versions would normally break RR in bad ways,
    but this lets us show a nice error message

--- PulsarEngine/RetroRewindChannel.cpp
+++ PulsarEngine/RetroRewindChannel.cpp
@@ -1,4 +1,5 @@
 #include <PulsarSystem.hpp>
+#include "Debug/Debug.hpp"
 #include "RetroRewindChannel.hpp"
 
 namespace Pulsar {
@@ -8,7 +9,7 @@ bool IsNewChannel() {
 }
 
 bool NewChannel_UseSeparateSavegame() {
-    return *reinterpret_cast<u8*>(RRC_BITFLAGS_ADDRESS) & RRC_BITFLAG_SEPARATE_SAVEGAME == RRC_BITFLAG_SEPARATE_SAVEGAME;
+    return (*reinterpret_cast<u8*>(RRC_BITFLAGS_ADDRESS) & RRC_BITFLAG_SEPARATE_SAVEGAME) == RRC_BITFLAG_SEPARATE_SAVEGAME;
 }
 
 void NewChannel_SetLoadedFromRRFlag() {
@@ -17,6 +18,21 @@ void NewChannel_SetLoadedFromRRFlag() {
 
 void NewChannel_SetCrashFlag() {
     *reinterpret_cast<u8*>(RRC_BITFLAGS_ADDRESS) |= RRC_BITFLAG_RR_CRASHED;
+}
+
+void NewChannel_Init() {
+    u32 channelVersion = *reinterpret_cast<u32*>(RRC_ABI_VERSION_ADDRESS);
+    u32 requiredVersion = RRC_ABI_VERSION;
+
+    // Make sure the channel is compatible with this Code.pul
+    if (channelVersion != requiredVersion) {
+        char message[256];
+        snprintf(message, sizeof(message), "This version of Retro Rewind is incompatible with the version of the channel (abi%d != abi%d).\n"
+            "You can usually fix this by updating both RR and the channel to the latest version.", channelVersion, requiredVersion);
+        Debug::FatalError(message);
+    }
+
+    NewChannel_SetLoadedFromRRFlag();
 }
 
 }  // namespace Pulsar
--- PulsarEngine/RetroRewindChannel.hpp
+++ PulsarEngine/RetroRewindChannel.hpp
@@ -1,11 +1,19 @@
 #ifndef _RETRO_REWIND_CHANNEL_
 #define _RETRO_REWIND_CHANNEL_
 
+#include <kamek.hpp>
+
 namespace Pulsar {
 
 // Signature written by the new launcher.
 #define RRC_SIGNATURE_ADDRESS 0x817ffff8
+#define RRC_ABI_VERSION_ADDRESS 0x817ffffc
 #define RRC_SIGNATURE 0xDEADBEEF
+
+// The "ABI version" of the new channel; incremented each time a change is necessary that would break compatibility
+// (for example moving addresses of fixed data elsewhere).
+// This needs to match the version declared on the channel side, or otherwise an error is shown.
+#define RRC_ABI_VERSION 1
 
 // IMPORTANT: Always keep these bitflags in sync with the channel's code when adding new ones so they don't conflict!
 #define RRC_BITFLAGS_ADDRESS 0x817ffff0
@@ -19,6 +27,7 @@ bool NewChannel_UseSeparateSavegame();
 void NewChannel_SetLoadedFromRRFlag();
 // This loads the channels' crash handler if set and the channel is called back into.
 void NewChannel_SetCrashFlag();
+void NewChannel_Init();
 
 }  // namespace Pulsar
 
