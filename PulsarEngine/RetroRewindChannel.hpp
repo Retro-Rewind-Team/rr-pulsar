50781863ed0335361db80f9121e8dbab0ede3563
Jacherr <jwc2002@outlook.com>
2026-04-16 18:15:11 +0100

    use ephemeral files for crashes and loaded from rr for new channel

--- PulsarEngine/RetroRewindChannel.hpp
+++ PulsarEngine/RetroRewindChannel.hpp
@@ -18,15 +18,14 @@ namespace Pulsar {
 // IMPORTANT: Always keep these bitflags in sync with the channel's code when adding new ones so they don't conflict!
 #define RRC_BITFLAGS_ADDRESS 0x817ffff0
 #define RRC_BITFLAG_SEPARATE_SAVEGAME 0x1
-#define RRC_BITFLAG_LOADED_FROM_RR 0x2
-#define RRC_BITFLAG_RR_CRASHED 0x4
+
+#define RRC_LOADED_FROM_RR_EPH_FILE_PATH "/RetroRewindChannel/.lfrr"
+#define RRC_CRASH_EPH_FILE_PATH "/RetroRewindChannel/.crash"
 
 bool IsNewChannel();
 bool NewChannel_UseSeparateSavegame();
-// If we call back into the channel, this flag indicates to skip some steps in the channel.
-void NewChannel_SetLoadedFromRRFlag();
-// This loads the channels' crash handler if set and the channel is called back into.
-void NewChannel_SetCrashFlag();
+void NewChannel_WriteLoadedFromRREphFile();
+void NewChannel_WriteCrashEphFile();
 void NewChannel_Init();
 
 }  // namespace Pulsar
