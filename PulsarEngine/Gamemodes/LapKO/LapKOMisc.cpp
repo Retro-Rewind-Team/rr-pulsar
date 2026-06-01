8eaad513164b9e70d7537dc4bd941834baa61c56
ZPL <158531259+ZacharyPL@users.noreply.github.com>
2026-06-01 17:25:25 -0500

    Remove redundant Wifi disconnect hooks

--- PulsarEngine/Gamemodes/LapKO/LapKOMisc.cpp
+++ PulsarEngine/Gamemodes/LapKO/LapKOMisc.cpp
@@ -19,9 +19,6 @@ static RaceFrameHook lapKoFrameHook(FrameUpdate);
 kmRuntimeUse(0x8053F3B8);  // Wifi Time Limit Expansion [Chadderz]
 kmRuntimeUse(0x8053F3BC);
 kmRuntimeUse(0x80521408);  // No Disconnect [Bully]
-kmRuntimeUse(0x8053EC94);
-kmRuntimeUse(0x8053EF6C);
-kmRuntimeUse(0x8053F0B4);
 kmRuntimeUse(0x8053F124);
 static void WifiEdits() {
     // Default is 5 minutes (300k Milliseconds)
@@ -30,17 +27,11 @@ static void WifiEdits() {
 
     // Default disconnect behavior
     kmRuntimeWrite32A(0x80521408, 0x38030001);
-    kmRuntimeWrite32A(0x8053EC94, 0x38040001);
-    kmRuntimeWrite32A(0x8053EF6C, 0x38030001);
-    kmRuntimeWrite32A(0x8053F0B4, 0x38030001);
     kmRuntimeWrite32A(0x8053F124, 0x38030001);
 
     #ifdef RR_TESTS
     // Disable disconnects from being idle
     kmRuntimeWrite32A(0x80521408, 0x38000000);
-    kmRuntimeWrite32A(0x8053EC94, 0x38000000);
-    kmRuntimeWrite32A(0x8053EF6C, 0x38000000);
-    kmRuntimeWrite32A(0x8053F0B4, 0x38000000);
     kmRuntimeWrite32A(0x8053F124, 0x38000000);
     #endif
 
@@ -56,9 +47,6 @@ static void WifiEdits() {
 
     // Disable disconnects from being idle
     kmRuntimeWrite32A(0x80521408, 0x38000000);
-    kmRuntimeWrite32A(0x8053EC94, 0x38000000);
-    kmRuntimeWrite32A(0x8053EF6C, 0x38000000);
-    kmRuntimeWrite32A(0x8053F0B4, 0x38000000);
     kmRuntimeWrite32A(0x8053F124, 0x38000000);
 }
 static SectionLoadHook WifiEditsHook(WifiEdits);
