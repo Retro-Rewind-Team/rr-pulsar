711f4e06de1edb2be843dd9d9a21abf20a5383fa
ZPL <158531259+zachpl-mk@users.noreply.github.com>
2026-06-23 21:06:55 -0500

    Bump Retro Rewind internal version

--- PulsarEngine/PulsarSystem.cpp
+++ PulsarEngine/PulsarSystem.cpp
@@ -555,7 +555,7 @@ kmRegionWrite32(0x80604094, 0x4800001c, 'E');
 kmWrite32(0x800017D0, 0x0A);
 
 // Retro Rewind Internal Version
-kmWrite32(0x800017D4, 6114);
+kmWrite32(0x800017D4, 6116);
 
 const char System::pulsarString[] = "/Pulsar";
 const char System::CommonAssets[] = "/CommonAssets.szs";
