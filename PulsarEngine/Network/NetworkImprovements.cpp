bd388baac41a5abde3d0d8aa30dedac703feb5f5
ZPL <158531259+zachpl-mk@users.noreply.github.com>
2026-07-06 18:55:35 -0500

    Remove 2-aid RACE packet network patch

--- PulsarEngine/Network/NetworkImprovements.cpp
+++ PulsarEngine/Network/NetworkImprovements.cpp
@@ -24,10 +24,5 @@ kmWrite32(0x800E77FC, 0x60000000);
 // Slower High Data Rate [MrBean35000vr, Chadderz]
 kmWrite32(0x80657EA8, 0x2804000C);
 
-// Send RACE packets to up to 2 aids per network tick [ZPL]
-kmWrite32(0x80657F5C, 0x7F9C1A14);  // add r28, r28, r3
-kmWrite32(0x80657FB4, 0x93590008);  // stw r26, 0x8(r25)
-kmWrite32(0x80657FB8, 0x2C1C0002);  // cmpwi r28, 2
-
 }  // namespace Network
 }  // namespace Pulsar
