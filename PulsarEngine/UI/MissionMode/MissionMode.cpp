6a8f0873bc502e3d765dc898d789e3ccac0ea2f6
TheBeefBai <55415096+TheBeefBai@users.noreply.github.com>
2026-07-06 13:50:39 +0100

    Fix Mission Mode menu loading

---
+++ PulsarEngine/UI/MissionMode/MissionMode.cpp
@@ -0,0 +1,69 @@
+#include <kamek.hpp>
+#include <runtimeWrite.hpp>
+#include <UI/MissionMode/MissionMode.hpp>
+#include <Gamemodes/MissionMode/MissionMode.hpp>
+#include <MarioKartWii/UI/Layout/Layout.hpp>
+#include <MarioKartWii/UI/Section/SectionMgr.hpp>
+
+namespace Pulsar {
+namespace UI {
+namespace MissionMode {
+
+static const char* GetMissionLevelLabelLayout(const char* lytName) {
+    if (strcmp(lytName, "common_w092_mission_level_label") == 0) {
+        return "zommon_w092_mission_level_label";
+    }
+    return lytName;
+}
+
+kmRuntimeUse(0x805e9f80);
+static void LoadPictureLayout(LayoutUIControl& control, const char* folderName, const char* lytName) {
+    typedef PictureLayout* (*AttachPictureLayout)(PictureLayoutList*, const char*, const char*);
+    const AttachPictureLayout attachPictureLayout = reinterpret_cast<AttachPictureLayout>(kmRuntimeAddr(0x805e9f80));
+    control.pictureLayout = attachPictureLayout(SectionMgr::sInstance->curSection->pictureLayoutList, folderName,
+            GetMissionLevelLabelLayout(lytName));
+}
+kmBranch(0x8063d9c0, LoadPictureLayout);
+
+u32 GetMissionButtonId(const Pages::SinglePlayer* page) {
+    return page->externControlCount - 2;
+}
+
+bool IsMissionButton(const Pages::SinglePlayer* page, u32 id) {
+    return id == GetMissionButtonId(page);
+}
+
+bool IsBTMRModeButton(const Pages::SinglePlayer* page, u32 id) {
+    return id == 3 || IsMissionButton(page, id);
+}
+
+u32 GetBTMRModeButtonBMG(const Pages::SinglePlayer* page, u32 id) {
+    return IsMissionButton(page, id) ? BMG_MISSION_MODE_BUTTON : BMG_BATTLE_MODE_BUTTON;
+}
+
+void CreateSinglePlayerPages(ExpSection& section) {
+    section.CreateAndInitPage(section, PAGE_MISSION_LEVEL_SELECT_UNUSED);
+    section.CreateAndInitPage(section, PAGE_MISSION_SELECT_SUB);
+    section.CreateAndInitPage(section, PAGE_MISSION_INFORMATION_PROMPT);
+    section.CreateAndInitPage(section, PAGE_MISSION_TUTORIAL);
+}
+
+void OnButtonSelect(Pages::SinglePlayer* page, PushButton& button, u32 hudSlotId) {
+    const s32 id = button.buttonId;
+    button.buttonId = 4;
+    page->Pages::SinglePlayer::OnExternalButtonSelect(button, hudSlotId);
+    button.buttonId = id;
+    page->bottomText->SetMessage(BMG_MISSION_MODE_BOTTOM);
+}
+
+bool OnButtonClick(Pages::SinglePlayer* page, PushButton& button, u32 hudSlotId) {
+    if (!IsMissionButton(page, button.buttonId)) return false;
+
+    Pulsar::MissionMode::PrepareMenuScenario();
+    page->LoadNextPageById(PAGE_MISSION_LEVEL_SELECT_UNUSED, button);
+    return true;
+}
+
+}  // namespace MissionMode
+}  // namespace UI
+}  // namespace Pulsar
