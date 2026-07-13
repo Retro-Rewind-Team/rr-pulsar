9b114d45bb0b2f3a615a91acf6518fe378228f12
TheBeefBai <55415096+TheBeefBai@users.noreply.github.com>
2026-07-13 18:35:00 +0100

    Refactor MissionMode code for clarity and reusability

--- PulsarEngine/UI/MissionMode/MissionMode.hpp
+++ PulsarEngine/UI/MissionMode/MissionMode.hpp
@@ -1,14 +1,11 @@
 #ifndef _PUL_UI_MISSIONMODE_
 #define _PUL_UI_MISSIONMODE_
-
 #include <kamek.hpp>
 #include <MarioKartWii/UI/Page/Menu/SinglePlayer.hpp>
 #include <UI/UI.hpp>
-
 namespace Pulsar {
 namespace UI {
 namespace MissionMode {
-
 u32 GetMissionButtonId(const Pages::SinglePlayer* page);
 bool IsMissionButton(const Pages::SinglePlayer* page, u32 id);
 bool IsBTMRModeButton(const Pages::SinglePlayer* page, u32 id);
@@ -17,9 +14,7 @@ void CreateSinglePlayerPages(ExpSection& section);
 void CreateRacePages(ExpSection& section);
 void OnButtonSelect(Pages::SinglePlayer* page, PushButton& button, u32 hudSlotId);
 bool OnButtonClick(Pages::SinglePlayer* page, PushButton& button, u32 hudSlotId);
-
 }  // namespace MissionMode
 }  // namespace UI
 }  // namespace Pulsar
-
 #endif
