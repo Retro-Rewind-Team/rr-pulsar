ef502b0352444940339079b689e1c8e229c68ace
TheBeefBai <55415096+TheBeefBai@users.noreply.github.com>
2026-07-13 20:17:58 +0100

    Add mission CPU population

--- PulsarEngine/UI/MissionMode/MissionMode.hpp
+++ PulsarEngine/UI/MissionMode/MissionMode.hpp
@@ -3,18 +3,21 @@
 #include <kamek.hpp>
 #include <MarioKartWii/UI/Page/Menu/SinglePlayer.hpp>
 #include <UI/UI.hpp>
+
 namespace Pulsar {
 namespace UI {
 namespace MissionMode {
-u32 GetMissionButtonId(const Pages::SinglePlayer* page);
-bool IsMissionButton(const Pages::SinglePlayer* page, u32 id);
-bool IsBTMRModeButton(const Pages::SinglePlayer* page, u32 id);
-u32 GetBTMRModeButtonBMG(const Pages::SinglePlayer* page, u32 id);
-void CreateSinglePlayerPages(ExpSection& section);
-void CreateRacePages(ExpSection& section);
-void OnButtonSelect(Pages::SinglePlayer* page, PushButton& button, u32 hudSlotId);
-bool OnButtonClick(Pages::SinglePlayer* page, PushButton& button, u32 hudSlotId);
+
+    u32 GetMissionButtonId(const Pages::SinglePlayer* page);
+    bool IsMissionButton(const Pages::SinglePlayer* page, u32 id);
+    bool IsBTMRModeButton(const Pages::SinglePlayer* page, u32 id);
+    u32 GetBTMRModeButtonBMG(const Pages::SinglePlayer* page, u32 id);
+    void CreateSinglePlayerPages(ExpSection& section);
+    void CreateRacePages(ExpSection& section);
+    void OnButtonSelect(Pages::SinglePlayer* page, PushButton& button, u32 hudSlotId);
+    bool OnButtonClick(Pages::SinglePlayer* page, PushButton& button, u32 hudSlotId);
 }  // namespace MissionMode
+
 }  // namespace UI
 }  // namespace Pulsar
 #endif
