952501bf06ed6d070a9a33558b25e4b3371b7616
TheBeefBai <55415096+TheBeefBai@users.noreply.github.com>
2026-07-13 20:17:58 +0100

    Add mission CPU population

--- PulsarEngine/Gamemodes/MissionMode/MissionMode.hpp
+++ PulsarEngine/Gamemodes/MissionMode/MissionMode.hpp
@@ -1,9 +1,15 @@
 #ifndef _PUL_MISSIONMODE_
 #define _PUL_MISSIONMODE_
 #include <kamek.hpp>
+
+class RacedataScenario;
+
 namespace Pulsar {
 namespace MissionMode {
+
+void PopulateMissionCPUs(RacedataScenario& scenario);
 void PrepareMenuScenario();
+
 }  // namespace MissionMode
 }  // namespace Pulsar
 #endif
