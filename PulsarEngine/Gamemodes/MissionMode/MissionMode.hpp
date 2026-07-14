f56be2dc73f0271c14cd221b78bdf67077af5267
TheBeefBai <55415096+TheBeefBai@users.noreply.github.com>
2026-07-14 12:17:08 +0100

    Add mission support with custom items

--- PulsarEngine/Gamemodes/MissionMode/MissionMode.hpp
+++ PulsarEngine/Gamemodes/MissionMode/MissionMode.hpp
@@ -7,6 +7,18 @@ class RacedataScenario;
 namespace Pulsar {
 namespace MissionMode {
 
+enum MissionFeatureFlag {
+    ITEM_MODE_OVERRIDE = 1 << 0,
+    CUSTOM_ITEMS_OVERRIDE = 1 << 1,
+    ENGINE_200CC = 1 << 2,
+    ENGINE_500CC = 1 << 3
+};
+
+bool IsMissionScenario(const RacedataScenario& scenario);
+bool HasMissionFeature(const RacedataScenario& scenario, MissionFeatureFlag feature);
+u8 GetMissionItemMode(const RacedataScenario& scenario);
+u32 GetMissionCustomItems(const RacedataScenario& scenario);
+void ApplyMissionScenarioSettings(RacedataScenario& scenario);
 void PopulateMissionCPUs(RacedataScenario& scenario);
 void PrepareMenuScenario();
 
