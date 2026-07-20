1f861534d244f43b4eefc8587916e859410d642f
ZPL <158531259+zachpl-mk@users.noreply.github.com>
2026-07-20 13:03:42 -0500

    Bump internal version number

--- PulsarEngine/PulsarSystem.cpp
+++ PulsarEngine/PulsarSystem.cpp
@@ -618,7 +618,7 @@ kmRegionWrite32(0x80604094, 0x4800001c, 'E');
 kmWrite32(0x800017D0, 0x0A);
 
 // Retro Rewind Internal Version
-kmWrite32(0x800017D4, 6116);
+kmWrite32(0x800017D4, 612);
 
 const char System::pulsarString[] = "/Pulsar";
 const char System::CommonAssets[] = "/CommonAssets.szs";
