29e76d4cd051f16d53a0470d223d5c037eaa59e9
ZPL <158531259+zachpl-mk@users.noreply.github.com>
2026-09-01 14:57:21 -0500

    Bump version

--- PulsarEngine/PulsarSystem.cpp
+++ PulsarEngine/PulsarSystem.cpp
@@ -635,7 +635,7 @@ BootHook RemoveESRBHook(removeESRB, 0);
 kmWrite32(0x800017D0, 0x0A);
 
 // Retro Rewind Internal Version
-kmWrite32(0x800017D4, 6124);
+kmWrite32(0x800017D4, 6125);
 
 const char System::pulsarString[] = "/Pulsar";
 const char System::CommonAssets[] = "/CommonAssets.szs";
